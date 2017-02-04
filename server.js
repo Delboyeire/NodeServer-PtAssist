e4e85cc31160f66b4424252f0673a28af0d6bcf8
Delboyeire <derekr31@hotmail.com>
2017-02-04 12:36:54 +0000

    cors

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var app      = express();
 var mongoose = require('mongoose');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var cors = require('cors');
  
 var databaseConfig = require('./config/database');
 var router = require('./app/routes');
@@ -26,8 +25,6 @@ mongoose.connect(databaseConfig.url);
  
 app.listen(process.env.PORT || 8080);
 console.log("App listening on port 8080");
-app.options('*', cors()); // include before other routes
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
 app.use(bodyParser.json()); // Send JSON responses
 app.use(logger('dev')); // Log requests to API using morgan
